24d743106ea991c2c18c35b5d502a8ab37404925
StevePhuc <stevephuc.dev@gmail.com>
2019-04-26 14:25:52 +0300

    find user

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,10 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -16,7 +20,18 @@ passport.use(
             // console.log('accessToken', accessToken);
             // console.log('refresh token', refreshToken);
             // console.log('profile', profile);
-            new User({ googleId: profile.id }).save().then(() => console.log('meow'));
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    // we already have a record with the given profile ID
+                    // null - evething ok, here is the user - existingUser
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id }).save().then(user => {
+                        console.log('meow');
+                        done(null, user);
+                    });
+                }
+            });
         }
     )
 );
